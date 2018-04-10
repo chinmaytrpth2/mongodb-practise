88f66624370f6a481b35b0e40d4dccd92973d197
Work <Work>
2018-04-10 18:38:49 +0530

    Added a get route to get the todo by its id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,4 @@
+var {moongoose} = require('./../server/db/mongoose');
+var {Todo} = require('./../server/models/todos');
+
+var id = '5acc8f676730c21944f4decf';
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todos');
@@ -28,6 +29,25 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 })
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    // Checking if the ID is valid!
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    // NOTE! -> Using return helps in stopping the execution of the function if anything gets wrong or if it gets executed
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo})
+    }, (e) => {
+        res.status(404).send();
+    });
+
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port:3000");
